dbca6c32c2e9bdb9796929d695d9295ea2a2222b
Evgeny <e.shingarev@mail.ru>
2022-04-18 14:47:23 +0300

    random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int namberA = 3;
-int namberB = 5;
+﻿int namberA = new Random().Next(1, 10);
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
 Console.WriteLine(namberA+namberB);
 //или, что равнозначно верхнему
 int resault = namberA + namberB;
