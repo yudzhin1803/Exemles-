0628d35618f1fa98faa8102137ba9d9affc18c77
Evgeny <e.shingarev@mail.ru>
2022-04-19 18:03:06 +0300

    .\Example010_MethodArray\

--- Example009_IntroArrey/Program.cs
+++ Example009_IntroArrey/Program.cs
@@ -8,9 +8,8 @@
 //             0   1   2   3   4   5   6   7   8 
 int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
 
-var result = Max(
-    Max(array[0], (array[1], (array[2]),
+var result = Max( Max(array[0], (array[1], (array[2]),
     Max(array[3], (array[4], (array[5]),
-    Max(array[6], (array[7], (array[8])
-);
+    Max(array[6], (array[7], (array[8]));
+
     Console.WriteLine(result);
