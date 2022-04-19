29a9431abfe49df8804cf23836bc7ef7c504ccda
Evgeny <e.shingarev@mail.ru>
2022-04-19 16:25:41 +0300

    Example 008 IntroMethod

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,9 +1,9 @@
 ﻿int Max(int arg1, int arg2, int arg3);
 {
-int result = arg1;
-if(arg2> result) result = arg2;
-if(arg3> result) result = arg3;
-return result;
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
 }
 
 int a1 = 15;
@@ -19,6 +19,6 @@ int c3 = 313;
 int max1 = Max(a1, b1, c1);
 int max2 = Max(a2, b2, c2);
 int max3 = Max(a3, b3, c3);
-int max = max(max1, max2, max3);
+int max = Max(max1, max2, max3);
 
 Console.WriteLine(max);
